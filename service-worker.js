8a5c223a01f74aea223e063c54f7e9a72214ab4e
mornya <mornya78@gmail.com>
2019-05-30 11:44:53 +0900

    Updates

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    self.caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/favicon.ico',
+        '/index.html'
+      ])
+    })
+  )
+})
