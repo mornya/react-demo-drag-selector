dac704261834838cffefbec7676309576ee6722c
mornya <mornya78@gmail.com>
2020-02-18 23:12:40 +0900

    Updates

--- components/about/reducer.d.ts
+++
@@ -1,4 +0,0 @@
-declare var _default: <Base extends {
-    readonly pageView: number;
-}>(base?: Base | undefined, __1?: any) => Base;
-export default _default;
--- reducers.d.ts
+++
@@ -1,6 +0,0 @@
-declare const _default: import("redux").Reducer<import("redux").CombinedState<{
-    about: {
-        readonly pageView: number;
-    };
-}>, any>;
-export default _default;
